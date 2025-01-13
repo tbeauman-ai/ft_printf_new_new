d60a1d903951b254ba7ec2e71690d1cfa72c1acf
Theo Beaumanoir <tbeauman@paul-f3Ar4s5.clusters.42paris.fr>
2025-01-13 16:19:49 +0100

    premier push

---
+++ ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbeauman <tbeauman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 12:56:08 by tbeauman          #+#    #+#             */
+/*   Updated: 2025/01/13 12:57:24 by tbeauman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "./libft/libft.h"
+# include <stdlib.h>
+# include <stdarg.h>
+# include <unistd.h>
+
+int     ft_printf(const char *, ...)
+
+#endif
